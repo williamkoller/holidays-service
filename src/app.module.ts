1dca9aafbd1d7ccb57e2e8c8f45552f7043d4e28
william koller <wkoller25@gmail.com>
2020-12-21 00:50:27 -0300

    chore: add GraphQLModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { join } from 'path'
 import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
