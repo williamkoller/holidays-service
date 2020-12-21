136dffc0ea51da1302dcdd8013a19d545d0700c9
william koller <wkoller25@gmail.com>
2020-12-21 00:50:46 -0300

    feat: add UserResolver w/ findAllUsers

---
+++ src/user/user.resolver.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from '@nestjs/graphql'
+import { User } from './entity/user.entity'
+import { UserService } from './user.service'
+
+@Resolver()
+export class UserResolver {
+  constructor(private readonly userSerice: UserService) {}
+
+  @Query(() => [User])
+  async findAllUsers(): Promise<Array<User>> {
+    return this.userSerice.findAllUsers()
+  }
+}
