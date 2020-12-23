029e962cb623bfb9a7ffc4caa2c5b26f9eec6d04
william koller <wkoller25@gmail.com>
2020-12-22 22:35:37 -0300

    chore: add ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ module.exports = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
