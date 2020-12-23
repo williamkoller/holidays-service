205f659df7b9a29af3c1617958d8a14270b6cae0
william koller <wkoller25@gmail.com>
2020-12-23 00:23:43 -0300

    feat: add findUserById

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,5 +1,6 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { User } from '../entities/user.entity'
+import { CreateUserInput } from './dto/create-user.input'
 import { UserService } from './user.service'
 
 @Resolver()
@@ -7,7 +8,17 @@ export class UserResolver {
   constructor(private readonly userSerice: UserService) {}
 
   @Query(() => [User])
-  async findAllUsers(): Promise<Array<User>> {
-    return this.userSerice.findAllUsers()
+  async findAllUsers(): Promise<User[]> {
+    return await this.userSerice.findAllUsers()
+  }
+
+  @Query(() => User)
+  async findUserById(@Args('id') id: string): Promise<User> {
+    return await this.userSerice.findUserById(id)
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserInput): Promise<User> {
+    return await this.userSerice.createUser(data)
   }
 }
