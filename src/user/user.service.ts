d71167cdcbb7c260beb9641162056c21cc90a4f0
william koller <wkoller25@gmail.com>
2020-12-22 22:35:21 -0300

    feat: add import User

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import { User } from './entity/user.entity'
+import { User } from '../entities/user.entity'
 
 @Injectable()
 export class UserService {
