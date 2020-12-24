0a150f8c46b8c359eb342db29f3691225397b138
william koller <wkoller25@gmail.com>
2020-12-24 18:17:11 -0300

    feat: add module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { join } from 'path'
 import { UserModule } from './user/user.module'
 import { ContinentModule } from './continent/continent.module'
 import { CountryModule } from './country/country.module'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { CountryModule } from './country/country.module'
     UserModule,
     ContinentModule,
     CountryModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
