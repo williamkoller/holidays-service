c8fb195f579d2825844ac313e4304cb0c1677208
william koller <wkoller25@gmail.com>
2020-12-25 00:01:35 -0300

    feat: add TypeOrmModule and JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common'
-import { AuthService } from './auth.service';
-import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service'
+import { AuthResolver } from './auth.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from 'src/entities/user/user.entity'
+import { JwtModule } from '@nestjs/jwt'
+import { UserService } from 'src/user/user.service'
+import { JwtStrategy } from './strategy/jwt.strategy'
 
 @Module({
-  providers: [AuthService, AuthResolver]
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: '1d',
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService, AuthResolver, UserService, JwtStrategy],
 })
 export class AuthModule {}
