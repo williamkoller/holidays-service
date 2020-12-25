a15f0e7aef66e65a831a0b534d49daa51a594ea3
william koller <wkoller25@gmail.com>
2020-12-25 00:01:52 -0300

    feat: add GraphQL login

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { AuthService } from './auth.service'
+import { AuthInput } from './dto/auth.input'
+import { AuthType } from './dto/auth.type'
 
-@Resolver()
-export class AuthResolver {}
+@Resolver('Auth')
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => AuthType)
+  public async login(@Args('data') data: AuthInput): Promise<AuthType> {
+    const response = await this.authService.validateUser(data)
+    return {
+      user: response.user,
+      token: response.token,
+    }
+  }
+}
